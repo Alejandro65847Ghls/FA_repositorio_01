98d3b43fb847769325d62d7132d1af9a853e4ec6
Alonso <alonso10052006@gmail.com>
2025-09-12 08:14:26 -0500

    Actualización

--- Semana1-Python/Semana1_Python.py
+++ Semana1-Python/Semana1_Python.py
@@ -4,4 +4,19 @@ def ejer1():
 
     print(f"\n{nombre}, bienvenido al FA de {carrera}")
 
-    ejer1()
+    def ejer2():
+       print("\Alejandro\"")
+
+       def ejer3():
+           num1 = int(input("Ingrese primer número:"))
+           num2 = int(input("Ingrese segundo número:"))
+
+           print("Suma: ", (num1+num2))
+           print("Resta: ", (num1-num2))
+           print("Multiplicación: ", (num1*num2))
+           print("División: ", (num1/num2))
+
+           ejer3()
+
+
+
