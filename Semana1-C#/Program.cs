5548e1a37c88ea249c571845a815bbf192cc1db3
Alonso <alonso10052006@gmail.com>
2025-09-12 08:30:37 -0500

    Otra actualización

--- Semana1-C#/Program.cs
+++ Semana1-C#/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Semana1_C_
@@ -11,14 +12,17 @@ namespace Semana1_C_
         static void Main(string[] args)
         {
             ejer1();
-                Console.ReadKey();
+            Console.ReadKey();
 
-            
-            
-                ejer2();
-                Console.ReadKey();
+
+
+            ejer2();
+            Console.ReadKey();
 
             ejer3();
+            Console.ReadKey();
+
+            ejer4();
             Console.ReadKey();
 
         }
@@ -60,8 +64,22 @@ namespace Semana1_C_
             Console.WriteLine("División: " + divi);
         }
 
+        static void ejer4()
+        {
+            Console.Write("Ingrese un número decimal: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            double raiz2 = Math.Sqrt(num);
+            int redo = (int)Math.Round(num, 0);
+            double cubo = Math.Pow(num, 3);
+            double raiz3 = Math.Pow(num, 1 / 3d);
+
+            Console.WriteLine("\nRaiz2: " + raiz2);
+            Console.WriteLine("Redondeado: " + redo);
+            Console.WriteLine("Al cubo: " + cubo);
+            Console.WriteLine("\nRaiz3: " + raiz3);
 
 
-
+        }
     }
 }
